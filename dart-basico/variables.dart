3aa289c708d097bebfa205c58c6cdfe71d17458c
gmcruzgonzabay <gmcruzgonzabay@gmail.com>
2026-07-23 21:56:40 -0500

    Declaracion de variables
    En esta sesion vimos como declarar variables, como imprimir, diferencias entre var y String, int, crear un tipo de dato double

---
+++ dart-basico/variables.dart
@@ -0,0 +1,61 @@
+void main() {
+  
+  final String nombre= ' Tony'; // si utilizo la palabra reservada final estoy 
+                                //declarando una constante
+  
+  //final es un valor fijo y no cambia
+  // const valor fijo antes de ejecutarse
+  //TAREA: declare un const y que sucede si cambia el valor;
+  
+   String apellido= 'Stark'; // Dart es sensible a mayusculas y minusculas
+  
+  
+  double salario= 1000.01;
+  
+  
+  
+  
+  int edad =30; // que es numerico
+  
+  // inicializar una variable significa reservar un espacio en memoria
+  
+  
+  print('$nombre');
+  
+  print(edad);
+  
+  print('$nombre $apellido su salario es $salario');
+  
+  //nombre= 'Juan'; Una variable final o constante no puede cambiar su valor
+  
+  
+  //var edad1= '31'; // que es String o texto
+  
+  
+  // Inferencia de tipo cuando uso el tipo de dato var
+  
+  
+  //String nombre1=''; // variable de tipo Texto o String
+  //int numero1= '20';  // Variable de tipo numerico 
+  
+ 
+ /* 
+  * Bloque de comentario
+  *
+  * 
+  * 
+  * 
+  *  */
+  
+  // Esto es un comentario en una linea
+  
+  //print("Hola Mundo!!!");
+  
+  //Debug console nos puede aparecer; Warning o Errores 
+  // Void main es una funcion que no devuelve nada, 
+  //un void main es similar a null? No.... nulo es un tipo de datos dentro de dart... y el main no devuelve 
+  // nada  .... hay funciones que si pueden usar un return null:
+  // Los tipos de datos que podemos tener aqui en dart, String, int, var, bool .... null
+ 
+
+}
