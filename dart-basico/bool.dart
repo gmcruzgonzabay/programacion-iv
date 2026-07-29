e17e73a1cb3741aea06b0ce42f87b7e7742fe1da
gmcruzgonzabay <gmcruzgonzabay@gmail.com>
2026-07-28 22:02:41 -0500

    Bool
    Manejo de booleanos y uso de nulos

---
+++ dart-basico/bool.dart
@@ -0,0 +1,22 @@
+void main() 
+{
+    bool? isActive= null;
+    
+  
+    String? nombre='Jose';
+  
+  if(isActive == null)
+  {
+    print('Esta variable es nula');
+    print(nombre);
+  }
+  else
+  {
+        print('Esta inactivo');
+
+  }
+  
+  
+
+
+}
